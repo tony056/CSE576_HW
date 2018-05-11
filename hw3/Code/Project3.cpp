f29baae52f0f068ca031462ab55e4a8c70b7b2ef
Tony Tung <tony61507@gmail.com>
2018-05-10 19:46:49 -0700

    added final Project3.cpp

--- hw3/Code/Project3.cpp
+++ hw3/Code/Project3.cpp
@@ -219,8 +219,61 @@ std::vector<double*> MainWindow::ExtractFeatureVector(QImage image)
     // The resultant image of Step 2 is 'nimg', whose values range from 1 to num_regions
 
     // WRITE YOUR REGION THRESHOLDING AND REFINEMENT CODE HERE
+    int validRegions = 0;
+    int threshold = 1000;
+    // for(int i = 0; i < num_regions; i++){
+    //     if(nimg[i] < threshold)
+    //         lostRegions++;
+    // }
+    // num_regions -= lostRegions;
+    // for()
+    int * region_idx;
+    region_idx = (int *)malloc((num_regions) * sizeof(int));
+    for(int i = 0; i < num_regions; i++)
+        region_idx[i] = 0;
+    for(int i = 0; i < w; i++){
+        for(int j = 0; j < h; j++){
+            region_idx[nimg[j*w + i]]++;
+        }
+    }
 
+    for(int i = 0; i < num_regions; i++){
+        if(region_idx[i] > threshold)
+            validRegions++;
+    }
+    qDebug("region_num = %d, lost_regions = %d\n",num_regions,validRegions);
 
+    int * region_table;
+    region_table = (int *)malloc((validRegions)*sizeof(int));
+    for(int i = 0; i < validRegions; i++)
+        region_table[i] = 0;
+
+    qDebug("created region table\n ");
+    int counter = 0;
+    for(int i = 0; i < num_regions; i++){
+        if(region_idx[i]>threshold){
+            region_table[counter] = i;
+            counter++;
+        }
+    }
+    qDebug("creating region_set");
+    vector<vector<pair<int, int>>> region_set;
+    for(int i = 0; i < validRegions; i++){
+        vector<pair<int, int>> pixel_set;
+        for(int j = 0; j < h; j++){
+            for(int k = 0; k < w; k++){
+                if((nimg[j*w+k]==region_table[i])&&(region_idx[nimg[j*w+k]]>threshold)){
+                    pixel_set.push_back(make_pair(j, k));
+                }
+            }
+        }
+        region_set.push_back((pixel_set));
+    }
+
+    // for(int i = 0; i < validRegions; i++){
+    //     qDebug("%d region, %d pixels\n",i,region_set[i].size());
+    // }
+    // qDebug("n: %d", num_regions);
     /********** STEP 3 **********/
 
 
@@ -230,30 +283,168 @@ std::vector<double*> MainWindow::ExtractFeatureVector(QImage image)
     // Extract the feature vector of each region
 
     // Set length of feature vector according to the number of features you plan to use.
-    featurevectorlength = 4;
+    featurevectorlength = 14;
 
     // Initializations required to compute feature vector
 
     std::vector<double*> featurevector; // final feature vector of the image; to be returned
-    double **features = new double* [num_regions]; // stores the feature vector for each connected component
-    for(int i=0;i<num_regions; i++)
+    double **features = new double* [validRegions]; // stores the feature vector for each connected component
+    for(int i=0;i<validRegions; i++)
         features[i] = new double[featurevectorlength](); // initialize with zeros
 
     // Sample code for computing the mean RGB values and size of each connected component
 
-    for(int r=0; r<h; r++)
-        for (int c=0; c<w; c++)
+    for(int i=0; i<validRegions; i++){
+        for (int j=0; j<region_set[i].size(); j++)
         {
-            features[nimg[r*w+c]-1][0] += 1; // stores the number of pixels for each connected component
-            features[nimg[r*w+c]-1][1] += (double) qRed(image.pixel(c,r));
-            features[nimg[r*w+c]-1][2] += (double) qGreen(image.pixel(c,r));
-            features[nimg[r*w+c]-1][3] += (double) qBlue(image.pixel(c,r));
+            int r = region_set.at(i).at(j).first;
+            int c = region_set.at(i).at(j).second;
+            features[i][0] += 1; // stores the number of pixels for each connected component
+            features[i][1] += (double) qRed(image.pixel(c,r));
+            features[i][2] += (double) qGreen(image.pixel(c,r));
+            features[i][3] += (double) qBlue(image.pixel(c,r));
         }
+    }
+    int size = 256;
+    double maxEnergy = 0;
+    double maxEntropy = 0;
+    double maxContrast = 0;
+    double maxHomogeneity = 0;
+
+    for(int i = 0; i < validRegions; i++){
+        int n = 0;
+        qDebug("new region: %d", i);
+        int max_x = -1, min_x = w+1;
+        int min_y = h+1, max_y = -1;
+
+        double coocMatrix [size][size];
+        for(int k = 0; k < size; k++){
+            for(int l = 0; l < size; l++){
+                // qDebug("matrix %d", coocMatrix[k][l]);
+                coocMatrix[k][l] = 0.0;
+                // qDebug("matrix %d", coocMatrix[k][l]);
+            }
+        }
+        for(int j = 0; j < region_set[i].size(); j++){
+            int r = region_set.at(i).at(j).first;
+            int c = region_set.at(i).at(j).second;
+
+            int dr = r + 1;
+            int dc = c + 1;
+
+            bool isInRegion = true;
+            if(dr >= h || dc >= w)
+                isInRegion = false;
+            // auto p = make_pair(dr, dc);
+            // if(std::find(region_set[i].begin(), region_set[i].end(), p) != region_set[i].end()){
+            //     isInRegion = true;
+            //     n++;
+            // }
+
+            if(isInRegion){
+                double rcGrayValue = (double) qRed(image.pixel(c,r)) + (double) qGreen(image.pixel(c,r)) + (double) qBlue(image.pixel(c,r));
+                rcGrayValue /= 3.0;
+                double drcGrayValue = (double) qRed(image.pixel(dc,dr)) + (double) qGreen(image.pixel(dc,dr)) + (double) qBlue(image.pixel(dc,dr));
+                drcGrayValue /= 3.0;
+                coocMatrix[(int)floor(rcGrayValue)][(int)floor(drcGrayValue)]++;
+                n ++;
+            }
+            if (r>max_y) max_y = r;
+            if (r<min_y) min_y = r;
+            if (c>max_x) max_x = c;
+            if (c<min_x) min_x = c;
+        }
+        //normalize the matrix
+        qDebug("n: %d", n);
+        for(int k = 0; k < size; k++){
+            for(int l = 0; l < size; l++){
+                // qDebug("matrix %d", coocMatrix[k][l]);
+                coocMatrix[k][l] /= n;
+                // qDebug("matrix %d", coocMatrix[k][l]);
+            }
+        }
+        double energy = 0;
+        double contrast = 0;
+        double entropy = 0;
+        double homogeneity = 0;
+        for(int k = 0; k < size; k++){
+            for(int l = 0; l < size; l++){
+                energy += (coocMatrix[k][l] * coocMatrix[k][l]);
+                contrast += ((k - l) * (k - l) * coocMatrix[k][l]);
+                if(coocMatrix[k][l] > 0)
+                    entropy -= (coocMatrix[k][l] * log2(coocMatrix[k][l] + 0.0));
+                homogeneity += (coocMatrix[k][l] / (1 + abs(k - l)));
+                if(energy > maxEnergy)
+                    maxEnergy = energy;
+                if(contrast > maxContrast)
+                    maxContrast = contrast;
+                if(homogeneity > maxHomogeneity)
+                    maxHomogeneity = homogeneity;
+                if(entropy > maxEntropy)
+                    maxEntropy = entropy;
+            }
+        }
+        features[i][4] = energy;
+        features[i][5] = contrast;
+        features[i][6] = entropy;
+        features[i][7] = homogeneity;
+        features[i][8] = (double)max_x/w;
+        features[i][9] = (double)max_y/h;
+        features[i][10] = (double)min_x/w;
+        features[i][11] = (double)min_y/h;
+        features[i][12] = (double)((min_x+max_x)/2)/w;
+        features[i][13] = (double)((min_y+max_y)/2)/h;
+    }
+
+    for(int i = 0; i < validRegions; i++){
+        features[i][4] /= maxEnergy;
+        features[i][5] /= maxContrast;
+        features[i][6] /= maxEntropy;
+        features[i][7] /= maxHomogeneity;
+    }
+
+    // for(int i = 0; i < validRegions; i++){
+    //     for(int j = 0; j < region_set[i].size(); j++){
+    //         // double r = region_set.at(i).at(j).first;
+    //         // double c = region_set.at(i).at(j).second;
+    //         // double gray_value = (double) qRed(image.pixel(c,r)) + (double) qGreen(image.pixel(c,r)) + (double) qBlue(image.pixel(c,r));
+    //         gray_value = (gray_value/3.0)/255.0;
+    //         features[i][4] += gray_value*gray_value; //energy
+    //         features[i][5] += gray_value*(r/h-c/w)*(r/h-c/w); //constrast
+    //         features[i][6] += -gray_value*log2(gray_value); //entropy
+    //         features[i][7] += gray_value/(1+fabs(r/h-c/w)); //homogeneity
+    //     }
+    //     features[i][4] /= region_set[i].size();
+    //     features[i][5] /= region_set[i].size();
+    //     features[i][6] /= region_set[i].size();
+    //     features[i][7] /= region_set[i].size();
+    // }
+    //
+    // for(int i = 0; i < validRegions; i++){
+    //     int max_x = -1, min_x = w+1;
+    //     int min_y = h+1, max_y = -1;
+    //     for(int j = 0; j < region_set[i].size(); j++){
+    //         int r = region_set.at(i).at(j).first;
+    //         int c = region_set.at(i).at(j).second;
+    //         if (r>max_y) max_y = r;
+    //         if (r<min_y) min_y = r;
+    //         if (c>max_x) max_x = c;
+    //         if (c<min_x) min_x = c;
+    //     }
+    //     features[i][8] = max_x;
+    //     features[i][9] = max_y;
+    //     features[i][10] = min_x;
+    //     features[i][11] = min_y;
+    //     features[i][12] = (min_x+max_x)/2;
+    //     features[i][13] = (min_y+max_y)/2;
+    // }
+
+
 
     // Save the mean RGB and size values as image feature after normalization
-    for(int m=0; m<num_regions; m++)
+    for(int m=0; m<validRegions; m++)
     {
-        for(int n=1; n<featurevectorlength; n++)
+        for(int n=1; n<4; n++)
             features[m][n] /= features[m][0]*255.0;
         features[m][0] /= (double) w*h;
         featurevector.push_back(features[m]);
@@ -262,6 +453,9 @@ std::vector<double*> MainWindow::ExtractFeatureVector(QImage image)
     // Return the created feature vector
     ui->progressBox->append(QString::fromStdString("***Done***"));
     QApplication::processEvents();
+    free(region_idx);
+    free(region_table);
+    region_set.clear();
     return featurevector;
 }
 
@@ -271,15 +465,24 @@ std::vector<double*> MainWindow::ExtractFeatureVector(QImage image)
 // Function that implements distance measure 1
 double distance1(double* vector1, double* vector2, int length)
 {
+    double dis = 0;
+    for(int i = 0; i < length; i++){
+        dis += sqrt((vector1[i] - vector2[i]) * (vector1[i] - vector2[i]));
+    }
     // default, for trial only; change according to your distance measure
-    return ((double) rand() / (double) RAND_MAX);
+    return dis;
 }
 
 // Function that implements distance measure 2
 double distance2(double* vector1, double* vector2, int length)
 {
     // default, for trial only; change according to your distance measure
-    return ((double) rand() / (double) RAND_MAX);
+    double dis = 0;
+    for(int i = 0; i < length; i++){
+        dis += abs((vector1[i] - vector2[i]));
+    }
+    return dis;
+    // return ((double) rand() / (double) RAND_MAX);
 }
 
 // Function to calculate the distance between two images
